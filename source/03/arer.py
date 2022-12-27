d0c8873b79add3f7e7ae97ba53873373469395fc
20156573 <keochanh62@gmail.com>
2022-12-27 22:03:17 +0700

    edit: edit src 03

--- source/03/arer.py
+++
@@ -1,5 +0,0 @@
-def display_info(first_name, last_name):
-    print('First Name:', first_name)
-    print('Last Name:', last_name)
-
-display_info(last_name='Cartman', 'Eric')
--- source/03/arer2.py
+++
@@ -1,3 +0,0 @@
-def display_info(first_name="Bob", last_name):
-    print('First Name:', first_name)
-    print('Last Name:', last_name)
