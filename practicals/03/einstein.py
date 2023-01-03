a05d734af6173ee3a6ba5efc694e3cb1e2f4498d
20156573 <keochanh62@gmail.com>
2023-01-03 21:55:43 +0700

    edit: edit prac 03

--- practicals/03/einstein.py
+++ practicals/03/einstein.py
@@ -1,8 +1,8 @@
 """
 Einstein
-Even if you haven't studied physics (recently or ever!), you might have heard that,
-wherein  represents energy (measured in Joules), represents mass (measured in kilograms),
-and represents the speed of light (measured approximately as 300000000 meters per second),
+Even if you haven't studied physics (recently or ever!), you might have heard that E=mc^2, 
+wherein E represents energy (measured in Joules), m represents mass (measured in kilograms),
+and c represents the speed of light (measured approximately as 300000000 meters per second),
 per Albert Einstein et al. Essentially, the formula means that mass and energy are equivalent.
 
 In a file called einstein.py,
